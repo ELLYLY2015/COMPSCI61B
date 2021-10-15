93a157d75738cc0890d9eee71e13d2a7d11cc159
ELLYLY2015 <89615806+ELLYLY2015@users.noreply.github.com>
2021-10-14 20:49:06 -0700

    Add files via upload

---
+++ HW/HW5/Nybbles.java
@@ -0,0 +1,67 @@
+/** Represents an array of integers each in the range -8..7.
+ *  Such integers may be represented in 4 bits (called nybbles).
+ *  @author
+ */
+public class Nybbles {
+
+    /** Maximum positive value of a Nybble. */
+    public static final int MAX_VALUE = 7;
+
+    /** Return an array of size N. 
+    * DON'T CHANGE THIS.*/
+    public Nybbles(int N) {
+        _data = new int[(N + 7) / 8];
+        _n = N;
+    }
+
+    /** Return the size of THIS. */
+    public int size() {
+        return _n;
+    }
+
+    /** Return the Kth integer in THIS array, numbering from 0.
+     *  Assumes 0 <= K < N. */
+    public int get(int k) {
+        if (k < 0 || k >= _n) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            int index = k/8;
+            int nybbleIndex = k%8;
+            int c = _data[index] >> 4 * nybbleIndex;
+            c &= 15;
+            if(c >= 8) {
+                return c - 16;
+            } else {
+                return c;
+            }
+        }
+    }
+
+    /** Set the Kth integer in THIS array to VAL.  Assumes
+     *  0 <= K < N and -8 <= VAL < 8. */
+    public void set(int k, int val) {
+        if (k < 0 || k >= _n) {
+            throw new IndexOutOfBoundsException();
+        } else if (val < (-MAX_VALUE - 1) || val > MAX_VALUE) {
+            throw new IllegalArgumentException();
+        } else {
+            int index = k/8;
+            int nybbleIndex = k%8;
+            int num = _data[index];
+            if(val < 0) {
+                val += 16;
+            }
+            //System.out.println(Integer.toBinaryString(val));
+            val <<= nybbleIndex*4;
+            //System.out.println(Integer.toBinaryString(val));
+            num |= val;
+            _data[index] = num;
+        }
+    }
+
+    /** DON'T CHANGE OR ADD TO THESE.*/
+    /** Size of current array (in nybbles). */
+    private int _n;
+    /** The array data, packed 8 nybbles to an int. */
+    private int[] _data;
+}
